0f3eb1a6559a5c7e9bd17d2eda726eddd05a9da1
RT_23 <69785556+23maria@users.noreply.github.com>
2021-10-14 13:35:54 +0530

    Add files via upload

---
+++ C/fifo.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,a[50],frame[10],no,k,avail,count=0;
+	printf("\n enter no of pages");
+	scanf("%d",&n);
+	printf("\nenter reference string:");
+	for(i=1;i<=n;i++)
+		scanf("%d",&a[i]);
+	printf("\n enter no of frames:");
+	scanf("%d",&no);
+	for(i=0;i<no;i++)
+		frame[i]=-1;
+	j=0;
+	printf("\trefstring \t page frame\n");
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t\t",a[i]);
+		avail=0;
+		for(k=0;k<no;k++)
+				if(frame[k]==a[i])
+					avail=1;
+		if(avail==0)
+		{
+			frame[j]=a[i];
+			j=(j+1)%no;
+			count++;
+			for(k=0;k<no;k++)
+					printf("%d\t",frame[k]);
+		}
+		printf("\n");
+	}
+	printf("page fault is %d",count);
+	return 0;
+}					
